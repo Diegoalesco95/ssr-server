5ece7da7ca3931503ea23210d9dd8c0d740c291a
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-03-31 14:13:29 -0500

    Comunicación máquina a máquina

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const boom = require('@hapi/boom');
+const cookieParser = require('cookie-parser');
+const axios = require('axios');
 
-const { config } = require("./config");
+const { config } = require('./config');
 
 const app = express();
 
 // body parser
 app.use(express.json());
+app.use(cookieParser());
 
-app.post("/auth/sign-in", async function(req, res, next) {
+// Basic strategy
+require('./utils/auth/strategies/basic');
 
+app.post('/auth/sign-in', async (req, res, next) => {
+  passsport.authenticate('basic', (error, data) => {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
+      req.login(data, { session: false }, async error => {
+        if (error) {
+          next(error);
+        }
+        res.cookie('token', token, {
+          httpOnly: !config.dev,
+          secure: !config.devm,
+        });
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
 });
 
-app.post("/auth/sign-up", async function(req, res, next) {
-
+app.post('/auth/sign-up', async function(req, res, next) {
+  const { bopy: user } = req;
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: 'post',
+      data: user,
+    });
+    res.status(201).json({ message: 'User created' });
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get("/movies", async function(req, res, next) {
+app.get('/movies', async function(req, res, next) {});
 
-});
+app.post('/user-movies', async function(req, res, next) {});
 
-app.post("/user-movies", async function(req, res, next) {
-
-});
-
-app.delete("/user-movies/:userMovieId", async function(req, res, next) {
-
-});
+app.delete('/user-movies/:userMovieId', async function(req, res, next) {});
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
